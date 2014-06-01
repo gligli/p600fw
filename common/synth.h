3d91987c6fad71cbe9ba4151f07cf29a716facd5
GliGli <gligli@sfxteam.org>
2014-06-01 09:42:58 +0200

    preparing 2.00 release

--- common/synth.h
+++ common/synth.h
@@ -6,7 +6,7 @@
 #include "hardware.h"
 
 //#define DEBUG
-#define RELEASE "1.99"
+#define RELEASE "Ver 2.00"
 
 #define UART_USE_HW_INTERRUPT // this needs an additional wire that goes from pin C4 to pin E4
 
