68c3cddb27f229c07aba485acab979d8549d1546
image-et-son <florian.merz@gmail.com>
2021-11-14 12:49:38 +0100

    Rearrangement of documentation
    Separattion of User Manaual (dependent on version) and Teensy
    Modification (independent from version). Added compilaton and package
    information for Linux.

--- common/synth.h
+++ common/synth.h
@@ -6,7 +6,7 @@
 #include "hardware.h"
 
 //#define DEBUG
-//#define RELEASE "Ver 2.00"
+//#define RELEASE "Ver 2.00" // will keep this as is until new release plan. Version 2.1 has never been officially released.
 
 #define UART_USE_HW_INTERRUPT // this needs an additional wire that goes from pin C4 to pin E4
 
--- mockup/print.h
+++
@@ -1,8 +0,0 @@
-#ifndef print_h__
-#define print_h__
-
-void print(const char *s);
-void phex(unsigned char c);
-void phex16(unsigned int i);
-
-#endif
